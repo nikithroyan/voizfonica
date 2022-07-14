1905e88d314d8b12626b59193cd3d3debeeb396d
ENG18CS0054 - Evans Nikith Royan <83712679+nikithroyan@users.noreply.github.com>
2022-07-14 15:39:47 +0530

    Delete voizfonica_django directory

--- voizfonica_django/website/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from website.models import prepaid_recharge
-
-
-class websiteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = prepaid_recharge
-        fields = '__all__'
